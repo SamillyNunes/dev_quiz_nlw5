56ed7b2b73783f8b9d0d50ad808b6de6e3372e30
SamillyNunes <samillynunes19@gmail.com>
2021-04-23 10:52:22 -0300

    Animating score card percentage

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/core/core.dart';
 
-class ChartWidget extends StatelessWidget {
+class ChartWidget extends StatefulWidget {
   final double percent;
 
   const ChartWidget({
@@ -12,32 +12,66 @@ class ChartWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _ChartWidgetState createState() => _ChartWidgetState();
+}
+
+class _ChartWidgetState extends State<ChartWidget>
+    with SingleTickerProviderStateMixin {
+  // tipo late: indicamos que so vamos inicializa-lo depois
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void _initAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+
+    // a principio essa Tween eh aquela animacao que vai "crescendo" de um valor inicial a um valor final
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: widget.percent,
+    ).animate(_controller);
+
+    _controller.forward(); // para a animacao "ir para a frente"
+  }
+
+  @override
+  void initState() {
+    _initAnimation();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: 80,
       width: 80,
-      child: Stack(
-        children: [
-          Center(
-            child: Container(
-              height: 80,
-              width: 80,
-              child: CircularProgressIndicator(
-                strokeWidth: 10,
-                value: this.percent,
-                backgroundColor: AppColors.chartSecondary,
-                valueColor:
-                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, _) => Stack(
+          children: [
+            Center(
+              child: Container(
+                height: 80,
+                width: 80,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  value: _animation.value,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
               ),
             ),
-          ),
-          Center(
-            child: Text(
-              "${(this.percent * 100).toInt()}%",
-              style: AppTextStyles.heading,
+            Center(
+              child: Text(
+                "${(_animation.value * 100).toStringAsFixed(0)}%",
+                style: AppTextStyles.heading,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
